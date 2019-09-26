79ba9ae697cd560a60680a4fa8d64476b8cb2890
tessiermelissa <tessiermelissa94@gmail.com>
2019-09-26 15:33:44 -0400

    tavaille du 26/09/19
    sauvegarde des contacts avec loggb dans un fichier json
    jouter d'un contract dans notre fichier

--- server-express.js
+++ server-express.js
@@ -24,6 +24,24 @@ nunjucks.configure('views', {
 });
 
 /**
+ * Importer le package 'lowdb'
+ * https://www.npmjs.com/package
+ */
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('data/contacts.json');
+const db = low(adapter);
+/**
+ * Pour récupérer les données POST, nous avons besoin
+ * de la librairie 'body-parser'. Elle nous
+ * permettra de manipuler les données 'POST' de la requète.
+ */
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/**
  * Les objets 'req' (requete) et 'res' (réponse)
  * sont exactement les mêmes que ceux fournit par Node.
  */
@@ -33,8 +51,37 @@ app.get('/', (req, res) => {
 });
 
 app.get('/contacts', (req, res) => {
+
+    // -- Récupérer la liste des contacts
+    const contactsDb = db.get('contacts'). value();
+
     // res.sendFile(__dirname + '/views/html/contacts.html');
-    res.render('html/contacts.html');
+    res.render('html/contacts.html',{
+        contacts: contactsDb
+    });
+});
+
+app.get('/ajouter-un-contact', (req, res) => {
+    res.render('html/ajouter-un-contact.html');
+});
+
+app.post('/ajouter-un-contact', (req, res) => {
+    /**
+     * lors de la soumission du formulaire avec
+     * la méthode POST, c'est cette fonction qui
+     * sera executée.
+     */
+    // console.log( req. body );
+    const contact = req.body;
+
+    // On ajoute le nouveau contact dans notre fichier JSON
+    db.get('contact')
+        .push(contact)
+        .write();
+
+    // On redirige l'utilisateur sur les contacts
+    res.redirect ('/contact');
+
 });
 
 app.get('/contact', (req, res) => {
